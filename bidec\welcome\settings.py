cc12fc34d55ef6adc3b69bd28ccc49ecc3726c36
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 19:22:00 +0500

    Add bidec\welcome\settings.py

---
+++ bidec\welcome\settings.py
@@ -0,0 +1,18 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'welcome',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+SECRET_KEY = 'secret_key_here'
